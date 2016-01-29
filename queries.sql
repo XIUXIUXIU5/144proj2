657df6579d0f7e8b952b95808e80beb24ab222ea
jwu0210 <jiwu@media.ucla.edu>
2016-01-29 13:24:23 -0800

    p2_test

--- queries.sql
+++ queries.sql
@@ -1,9 +1,7 @@
 select count(*) from ((select SellerID from Seller) Union (select BidderID from Bidder)) s;
 select count(*) from Item where BINARY Location = "New York";
 select count(*) from (select ItemID from ItemCategory group by ItemID Having count(Category) = 4 ) s;
-select ItemID from Item where Currently >= (select MAX(Currently) from (select * from Item where Ends >= "2001-12-20 00:00:01") s where ItemID IN (select ItemID from ItemBid));
 select a.ItemID from Item a, ItemBid s where a.ItemID = s.ItemID AND a.Ends >= "2001-12-20 00:00:01" AND a.Currently >= (select MAX(Currently) from Item i, ItemBid b where i.ItemID = b.ItemID AND i.Ends >= "2001-12-20 00:00:01");
 select count(*) from Seller where Rating > 1000;
-select count(*) from ((select SellerID from Seller) INTERSECT (select BidderID from Bidder)) s;
-select count(*) from Seller s, Bidder b where s.SellerID = b.BidderID;
+select count(*) from Seller, Bidder where SellerID = BidderID;
 select count(*) from (select distinct Category from ItemCategory where ItemID in (select distinct ItemID from ItemBid where Amount > 100)) s;
--- runLoad.sh
+++ runLoad.sh
@@ -2,8 +2,8 @@
 S2="$(mysql -N -s -e "select count(*) from information_schema.tables where table_schema='CS144';")" 
 if [ $S2 -gt 0 ]; 
 then
-	echo "drop"
-	mysql CS144 < drop.sql
+    echo "drop"
+mysql CS144 < drop.sql
 fi
 
 mysql CS144 < create.sql
